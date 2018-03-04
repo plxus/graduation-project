ba8660d427c061de6e6509903392c4f8f2e6d01e
plusxu <xplusxu@gmail.com>
2018-03-04 19:28:59 +0800

    3.4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+      // 调用 create 方法创建数据表，有两个参数，第一个是数据表名，第二个是 $table 的闭包。
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-md-12">
     <footer>
-      <p><small>&copy; 2018 知所<br />
+      <p><small>&copy; {{ date("Y") }} 知所<br />
       All Rights Reserved</small>
       </p>
     </footer>
