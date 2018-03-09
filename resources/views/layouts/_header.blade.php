f7446200580212e4638899f45bd089e37f0a2671
plusxu <xplusxu@gmail.com>
2018-03-09 11:35:36 +0800

    3.9

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,44 +10,50 @@
       </button>
       <a class="navbar-brand" href="{{ route('home') }}">知所</a>
     </div>
-
+    
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <form class="navbar-form navbar-left">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="搜索你感兴趣的内容">
-      </div>
-      <button type="submit" class="btn btn-default">搜索</button>
-    </form>
-    <ul class="nav navbar-nav">
-      <li><a href="{{ route('about') }}">关于</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      @auth
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="" title="用户头像"> <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li class="dropdown-header">登录为 {{ $user->name }}</li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">个人主页</a></li>
-            <li><a href="#">我的收藏</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">设置</a></li>
-            <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
-              document.getElementById('logout-form').submit();">注销</a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-              </form>
-            </li>
-          </ul>
-        </li>
-      @else
-        <li><a href="{{ route('register') }}">注册</a></li>
-        <li><a href="{{ route('login') }}">登录</a></li>
-      @endauth
-    </ul>
-    <!-- /.navbar-collapse -->
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="搜索你感兴趣的内容">
+        </div>
+        <button type="submit" class="btn btn-default">搜索</button>
+      </form>
+      <ul class="nav navbar-nav">
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        @auth
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-plus"></i>&nbsp;<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">新建知识清单</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="" title="用户头像">&nbsp;<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li class="dropdown-header">登录为 {{ $user->name }}</li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">个人主页</a></li>
+              <li><a href="#">我的收藏</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">设置</a></li>
+              <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">注销</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                </form>
+              </li>
+            </ul>
+          </li>
+        @else
+          <li><a href="{{ route('register') }}">注册</a></li>
+          <li><a href="{{ route('login') }}">登录</a></li>
+        @endauth
+      </ul>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
   </div>
-  <!-- /.container -->
-</div>
 </nav>
